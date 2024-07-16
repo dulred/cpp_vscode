4e6f3cbb54dc7ea2776afdef806b376b1a16ba4d
dulred <dulred@qq.com>
2024-07-16 09:32:55 +0800

    1

---
+++ build/CMakeFiles/my_cmake_exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/my_cmake_exe.dir/main.cpp.o"
+  "CMakeFiles/my_cmake_exe.dir/src/test.cpp.o"
+  "my_cmake_exe.pdb"
+  "my_cmake_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/my_cmake_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main (int argc , char const *argv[]){
+    int cc = 30;
+    int gg = 55;
+    std::cout <<"hello 123123" << std::endl;
+    std::cout <<"i love you" << std::endl;
+    return 0;
+} 
